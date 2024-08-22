65ded54e1331427a806f8882b30f1de8c669a4c0
Torion <tojo.ranto@gmail.com>
2024-08-22 12:32:30 +0200

    Exercices termines

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,11 @@
-//Votre code ici
+function add(a, b) {
+  let sum = Number(a) + Number(b); // caster => forcer le type d'une variable
+  return sum;
+}
 
- export default add;
+let int = 3;
+let float = 1.5;
+
+console.log(add(int, float));
+
+export default add;
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,15 @@
-// votre code ici
+function checkAge(age) {
+  if (age < 18) {
+    return "Vous êtes mineur.";
+  } else if (age >= 18 && age < 65) {
+    return "Vous êtes majeur.";
+  } else {
+    return "Vous êtes senior.";
+  }
+}
+
+console.log(checkAge(15));
+console.log(checkAge(30));
+console.log(checkAge(75));
 
 export default checkAge;
